12c7050164e95a05ece06afd37f67f0c5bbfb173
vrublevskyy <ivan.vrublevskyy@edu.uah.es>
2016-02-21 15:50:16 +0100

    new server

--- js/geo.js
+++ js/geo.js
@@ -1,6 +1,5 @@
 //---------------------- Global vars
 var map = L.map('map').setView([40.5126759, -3.3502846], 13);
-var myServer="http://router.project-osrm.org"
 var routeCoordinates={
   "origin":{
     "lat":null,
@@ -31,7 +30,7 @@ function getLocation(myCallback) {
 
 $('#send').click( function() {
     $.ajax({
-        url: 'http://localhost:3000/addFacultad',
+        url: 'http://www.paradisecity.me:3000/addFacultad',
         type: 'post',
         dataType: 'json',
         data: $('#sendForm').serialize(),
