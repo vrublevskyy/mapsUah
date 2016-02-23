25646672ed2bf958bb075fe3f44565345c82a7c8
vrublevskyy <ivan.vrublevskyy@edu.uah.es>
2016-02-23 23:32:09 +0100

    html sin href

--- js/page.js
+++ js/page.js
@@ -6,10 +6,10 @@ $(document).ready(function () {
     success: function(data) {
                console.log(data)
                for (var facultad in data) {
-                 $('#facultades').append("<div class=\"col-lg-3 col-md-4 col-xs-6 thumb onclick=setPoint(\"+data._id+\")> <a href=\"#"+data[facultad]._id+"\"><img src="+data[facultad].properties.imgSrc+" alt=\"\" /></a>  \
-                   <h4><a href=\"#"\">"+data[facultad].properties.name+"</a></h4> \
-                   <p>"+data[facultad].properties.info+"</p> \
-                 </div>");
+                 $('#facultades').append("<div class=\"col-lg-3 col-md-4 col-xs-6 thumb\" onclick=\"setPoint("+data[facultad]._id+")\"> \
+                 <img src="+data[facultad].properties.imgSrc+" alt=\"\" /></a>  \
+                 <h4><a href=\"#"\">"+data[facultad].properties.name+"</a></h4> \
+                 <p>"+data[facultad].properties.info+"</p></div>");
                }
 
              },
@@ -22,5 +22,10 @@ $(document).ready(function () {
 
 function setPoint (data){
     console.log(data)
-    
+
 }
+
+"<div class=\"col-lg-3 col-md-4 col-xs-6 thumb\" onclick=\"setPoint("+data[facultad]._id+")\"> \
+<img src="+data[facultad].properties.imgSrc+" alt=\"\" /></a>  \
+<h4><a href=\"#"\">"+data[facultad].properties.name+"</a></h4> \
+<p>"+data[facultad].properties.info+"</p></div>"
