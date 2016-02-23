76b3355ecaa94a195fc7ace93dc71b960a71169a
Ivan Vrublevskyy <vrublevskyy@users.noreply.github.com>
2016-02-23 17:31:21 +0100

    change url

--- js/page.js
+++ js/page.js
@@ -5,8 +5,8 @@ $.ajax({
     success: function(data) {
                console.log(data)
                for (var facultad in data) {
-                 $('#facultades').append("<div class=\"col-lg-3 col-md-4 col-xs-6 thumb\"> <a href=\"http://192.168.1.150:8082/manage.html#"+data[facultad]._id+"\"><img src="+data[facultad].properties.imgSrc+" alt=\"\" /></a>  \
-                   <h4><a href=\"http://192.168.1.150/manage.html/#"+data[facultad]._id+"\">"+data[facultad].properties.name+"</a></h4> \
+                 $('#facultades').append("<div class=\"col-lg-3 col-md-4 col-xs-6 thumb\"> <a href=\"http://ivr0.myqnapcloud.com/:8082/admin/manage.html#"+data[facultad]._id+"\"><img src="+data[facultad].properties.imgSrc+" alt=\"\" /></a>  \
+                   <h4><a href=\"http://ivr0.myqnapcloud.com/admin/manage.html/#"+data[facultad]._id+"\">"+data[facultad].properties.name+"</a></h4> \
                    <p>"+data[facultad].properties.info+"</p> \
                  </div>");
                }
