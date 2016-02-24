13c3d0cab5d2e68f7085dd465220c2f8c4a46723
vrublevskyy <ivan.vrublevskyy@edu.uah.es>
2016-02-24 00:40:17 +0000

    fucniona el boton

--- js/page.js
+++ js/page.js
@@ -6,13 +6,15 @@ $(document).ready(function () {
     success: function(data) {
                console.log(data)
                for (var facultad in data) {
-                 $('#facultades').append("<div id=\""+facultad+"\" class=\"col-lg-3 col-md-4 col-xs-6 thumb\"> <a><img src="+data[facultad].properties.imgSrc+"/></a>  \
+console.log(data[facultad]._id)
+
+                 $('#facultades').append("<div id=\""+facultad+"\" class=\"col-lg-3 col-md-4 col-xs-6 thumb\"> <a><img src="+data[facultad].properties.imgSrc+"></a>  \
                    <h4><a>"+data[facultad].properties.name+"</a></h4> \
                    <p>"+data[facultad].properties.info+"</p> \
-                   <button type=\"button\" class=\"btn btn-default btn-lg\" onclick=\"setPoint(\""+data[facultad]._id+"\")\" ></button> \
+                   <button type=\"button\" class=\"btn btn-default btn-lg\" onclick=\"setPoint('"+data[facultad]._id+"')\" >Buscar ruta</button> \
                  </div>");
                }
-
+	//	console.log(data[facultad]._id)
              },
     error: function(XMLHttpRequest, textStatus, errorThrown) {
         console.log("Status: " + textStatus); console.log("Error: " + errorThrown);
