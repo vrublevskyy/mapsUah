16b1e99fbd5a27e60e7db04d928a7274c99fbeb9
vrublevskyy <ivan.vrublevskyy@edu.uah.es>
2016-02-29 23:02:49 +0100

    correciones

--- js/page.js
+++ js/page.js
@@ -5,8 +5,8 @@ $.ajax({
     success: function(data) {
                console.log(data)
                for (var facultad in data) {
-                 $('#facultades').append("<div class=\"col-lg-3 col-md-4 col-xs-6 thumb\"> <a href=\"http://192.168.1.150:8082/manage.html#"+data[facultad]._id+"\"><img src="+data[facultad].properties.imgSrc+" alt=\"\" /></a>  \
-                   <h4><a href=\"http://192.168.1.150/manage.html/#"+data[facultad]._id+"\">"+data[facultad].properties.name+"</a></h4> \
+                 $('#facultades').append("<div class=\"col-lg-3 col-md-4 col-xs-8 thumb\"> <a href=\"http://www.paradisecity.me:8088/manage.html/#"+data[facultad]._id+"\"><img src="+data[facultad].properties.imgSrc+" alt=\"\" /></a>  \
+                   <h4><a href=\"http://www.paradisecity.me:8088/manage.html/#"+data[facultad]._id+"\">"+data[facultad].properties.name+"</a></h4> \
                    <p>"+data[facultad].properties.info+"</p> \
                  </div>");
                }
@@ -15,9 +15,5 @@ $.ajax({
     error: function(XMLHttpRequest, textStatus, errorThrown) {
         console.log("Status: " + textStatus); console.log("Error: " + errorThrown);
     }
-
-});
-
-$(document).ready(function () {
 
 });
